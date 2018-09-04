3d79057fc7cf3798de79c445ee3d6c66e00afe23
majorant <majorant@yandex.ru>
2018-09-04 13:29:58 +0300

    add logging, add config; add functionallity: convert input xls/ods file in csv file with five fields.

--- ods2csv.py
+++ ods2csv.py
@@ -1,20 +1,37 @@
 import os
-# import config
+import config
 import pyexcel as pe
 import csv
+import logging
+
 
 def main(base_dir):
     # Simply give a name to the Book class
-    book = pe.get_book(file_name=os.path.join(base_dir, "multiple-sheets-example.xls"))
-
-    for sheet in book:
-        # write sheet to csv file, with name like sheet name
-        with open(str(sheet.name) + '.csv', 'w', newline='') as csvfile:
-            swriter = csv.writer(csvfile, delimiter=',', quotechar = '\"',
-                                            quoting=csv.QUOTE_MINIMAL)
+    book = pe.get_book(file_name=os.path.join(base_dir, config.ppc_in))
+    # write sheet to csv file, with name like sheet name
+    with open(config.ppc_out, 'w', newline='') as csvfile:
+        swriter = csv.writer(csvfile, delimiter=',', quotechar = '\"',
+                                        quoting=csv.QUOTE_MINIMAL)
+        # fieldnames outup csv file
+        swriter.writerow(config.fieldnames)
+        for sheet in book:
+            try:
+                bts, ip = config.bts[sheet.name]
+            except KeyError as e:
+                logging.error('error in configuration, no key {} in config'.format(e))                
+                continue
             for row in sheet:
-                swriter.writerow(row)
+                if str(row[0]).isdigit():
+                    swriter.writerow([sheet.name, row[4], row[5], bts, ip])
 
 
 if __name__ == '__main__':
+    # confgi logging
+    logging.basicConfig(
+        filename="ods2csv.log",
+        level=logging.INFO,
+        # format="%(asctime)s [%(name)s] [%(levelname)s]: %(message)s"
+        format="%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s"
+    )
+    logging.info('start coverting...')
     main(os.getcwd())
